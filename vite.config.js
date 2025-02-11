fc3af676d715c7bdd7d9ad7baa440fd564d99855
Jon Fleming <jonfleming@hotmail.com>
2025-02-10 22:44:31 -0800

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { join, dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
+import dns from 'dns';
 
 const path = fileURLToPath(import.meta.url);
 console.log("meta", import.meta.url);
 console.log("path", path);
 console.log("root", join(dirname(path), "client"));
 
+dns.setDefaultResultOrder('verbatim');
+
 export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000
+  }
 };
